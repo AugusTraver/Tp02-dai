e7d3ac39bf530b17046de239eb754a31c4487076
Android Studio <devandroid@ort>
2026-03-16 09:03:26 -0300

    Hicimos hasta el 2

---
+++ src/Tp2.js
@@ -0,0 +1,17 @@
+/* Importo la constante PI y la función sumar del módulo matematica. */
+import {PI, sumar, multiplicar, dividir, restar} from '../src/modules/matematica.js';
+let total, numero1 = 10, numero2 = 20;
+console.clear();
+console.log(`La constante PI vale '${PI}'`); // Uso la constante PI importada.
+
+total = sumar(numero1, numero2);
+console.log(`sumar(${numero1}, ${numero2}) = ${total}`);
+
+total = restar(numero1, numero2);
+console.log(`restar(${numero1}, ${numero2}) = ${total}`);
+
+total = multiplicar(numero1, numero2);
+console.log(`multiplicar(${numero1}, ${numero2}) = ${total}`);
+
+total = dividir(numero1, numero2);
+console.log(`dividir(${numero1}, ${numero2}) = ${total}`);
